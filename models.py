6da15e16714968da48cb5f3a66118a85b11d3ba2
julius <s64jzimm@uni-bonn.de>
2026-07-08 23:54:27 +0200

    fixed paths after directory renaming

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
-import cmbNN_codes.functions as functions
+import cmdNN_codes.functions as functions
 
 # #Hyperparameters
 # LR = 1e-4
