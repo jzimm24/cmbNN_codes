b3d7add4c24ea72a25cf56fbf6b55516d95a8364
julius <s64jzimm@uni-bonn.de>
2026-08-29 00:55:47 +0200

    gan run

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ maps_configs = maps_config()
 # Training
 
 class training_config(NamedTuple):
-    model: str = "UNET"
+    model: str = "GAN"
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     NUM_EPOCHS = 16
     BATCH_SIZE = 32
@@ -90,10 +90,10 @@ class training_config(NamedTuple):
     IMG_DIM = 128
     DATA_FILE = "../data/realNoise128_10k_7rc_1WN_3f_2808.h5"
     TRAIN_SPLIT = 1-0.05
-    trained_model_name = "../models/unet_realNoise128_10k_7rc_1WN_3f_2808"
+    trained_model_name = "../models/gan_realNoise128_10k_7rc_1WN_3f_2808"
     crit = nn.BCEWithLogitsLoss() 
-    doc_path = "../outputs/docs/unet_realNoise128_10k_7rc_1WN_3f_2808.txt"
-    visualization = True
+    doc_path = "../outputs/docs/gan_realNoise128_10k_7rc_1WN_3f_2808.txt"
+    visualization = False
     visualization_file = "../outputs/realNoise128_10k_7rc_1WN_3f_2808_postNorm.png"
 
 training_configs = training_config()
